1d675b53ab48c3f4462780e39f93f9953433b0ca
TheMaara <68475422+TheMaara@users.noreply.github.com>
2022-02-11 10:21:28 +0300

    Works now-purple dots

---
+++ src/utilities.js
@@ -0,0 +1,27 @@
+//Drawing function
+export const drawHand = (predictons, ctx) => {
+    //check if we have predictons
+    if(predictons.length>0){
+        //loop through each prediction
+        predictons.forEach((prediction) => {
+            //Grab landmarks
+            const landmarks = prediction.landmarks;
+
+            //loop through landmarks and draw them
+            for (let i=0; i<landmarks.length; i++){
+                //Get x point
+                const x = landmarks[i][0];
+                //Get y point
+                const y = landmarks[i][1];
+                //Start drawing
+                ctx.beginPath();
+                ctx.arc(x, y, 5, 0, 3 * Math.PI);
+
+                //Set line color
+                ctx.fillStyle = "indigo";
+                ctx.fill();
+            }
+            
+        });
+    }
+};
